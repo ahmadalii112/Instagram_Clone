cf52e459f6d886e0018fa936e14711dd3b80a754
Ahmad Ali <ahmad.ali78643@gmail.com>
2021-08-08 22:56:50 +0500

    Authentication and Designing Frontend

---
+++ resources/views/home.blade.php
@@ -0,0 +1,40 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <!-- Profile Image-->
+        <!-- Row Start-->
+        <div class="row">
+            <div class="col-3 p-5">
+                <img class="rounded-circle" src="http://placehold.it/150x150" alt="Profile-Image">
+            </div>
+            <!-- Profile Info-->
+            <div class="clo-9 pt-5">
+                <div><h1>Ahmad Ali</h1></div>
+                <div class="d-flex">
+                    <div class="pr-5"><strong>153</strong> Posts</div>
+                    <div class="pr-5"><strong>23k</strong> Followers</div>
+                    <div class="pr-5"><strong>212</strong> Following</div>
+                </div>
+                <div class="pt-4 font-weight-bold">FreeCodeCamp</div>
+                <div>Lorem ipsum dolor sit sasdw das.</div>
+                <div><a href="https://instagram.com/AhmadAliii21">AhmadAliii21</a></div>
+            </div>
+        </div>
+        <!-- Row Ends-->
+        <!-- Posts -->
+        <!-- Row Start-->
+        <div class="row pt-4">
+            <div class="col-4">
+                <img class="w-100" src="http://placehold.it/300x300" alt="">
+            </div>
+            <div class="col-4">
+                <img class="w-100" src="http://placehold.it/300x300" alt="">
+            </div>
+            <div class="col-4">
+                <img  class="w-100" src="http://placehold.it/300x300" alt="">
+            </div>
+        </div>
+        <!-- Row Ends-->
+    </div>
+@endsection
